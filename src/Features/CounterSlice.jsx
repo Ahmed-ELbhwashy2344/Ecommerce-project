ffb02ec586049fd19b8bae41c15a4f765467ef33
ahmed mohamed <ahmed@emamil.com>
2026-01-05 21:47:35 +0200

    update

--- src/Features/CounterSlice.jsx
+++ src/Features/CounterSlice.jsx
@@ -1,34 +1,34 @@
-import { createSlice } from "@reduxjs/toolkit";
+// import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {
-  result: null,
-};
+// const initialState = {
+//   result: null,
+// };
 
-export const counterSlice = createSlice({
-  name: "Calcs",
-  initialState,
-  initialState,
-  reducers: {
-    add: (currentState, action) => {
-      console.log(action);
-      const sum =
-        Number(action.payload.firstNumber) +
-        Number(action.payload.secondNumber);
-      currentState.result = sum;
-    },
-    sub: (currentState, action) => {
-      const { firstNumber, secondNumber } = action.payload;
-      currentState.result = firstNumber - secondNumber;
-    },
-    mult: (currentState, action) => {
-      const { firstNumber, secondNumber } = action.payload;
-      currentState.result = firstNumber * secondNumber;
-    },
-    div: (currentState, action) => {
-      const { firstNumber, secondNumber } = action.payload;
-      currentState.result = firstNumber / secondNumber;
-    },
-  },
-});
-export const { add, sub, mult, div } = counterSlice.actions;
-export default counterSlice.reducer;
+// export const counterSlice = createSlice({
+//   name: "Calcs",
+//   initialState,
+//   initialState,
+//   reducers: {
+//     add: (currentState, action) => {
+//       console.log(action);
+//       const sum =
+//         Number(action.payload.firstNumber) +
+//         Number(action.payload.secondNumber);
+//       currentState.result = sum;
+//     },
+//     sub: (currentState, action) => {
+//       const { firstNumber, secondNumber } = action.payload;
+//       currentState.result = firstNumber - secondNumber;
+//     },
+//     mult: (currentState, action) => {
+//       const { firstNumber, secondNumber } = action.payload;
+//       currentState.result = firstNumber * secondNumber;
+//     },
+//     div: (currentState, action) => {
+//       const { firstNumber, secondNumber } = action.payload;
+//       currentState.result = firstNumber / secondNumber;
+//     },
+//   },
+// });
+// export const { add, sub, mult, div } = counterSlice.actions;
+// export default counterSlice.reducer;
--- src/Page/About/About.jsx
+++ src/Page/About/About.jsx
@@ -1,100 +1,107 @@
-import React, { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { add, div, mult, sub } from "../../Features/CounterSlice";
+// import React, { useState } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { add, div, mult, sub } from "../../Features/CounterSlice";
+
+// export default function About() {
+//   const [firstNumberInput, setFirstNumberInput] = useState(null);
+//   const [secondNumberInput, setSecondNumberInput] = useState(null);
+//   const [result, setResult] = useState(null);
+//   const resultState = useSelector((state) => {
+//     console.log("Hello", state);
+//     return state.counter.result;
+//   });
+//   const dispatech = useDispatch();
+
+//   function handleSum() {
+//     // const result = Number(firstNumberInput) + Number(secondNumberInput);
+//     // setResult(result);
+//     dispatech(
+//       add({
+//         firstNumber: firstNumberInput,
+//         secondNumber: secondNumberInput,
+//       })
+//     );
+//   }
+//   function handleSub() {
+//     dispatech(
+//       sub({
+//         firstNumber: firstNumberInput,
+//         secondNumber: secondNumberInput,
+//       })
+//     );
+//   }
+//   function handleMlti() {
+//     dispatech(
+//       mult({
+//         firstNumber: firstNumberInput,
+//         secondNumber: secondNumberInput,
+//       })
+//     );
+//   }
+//   function handleDiv() {
+//     dispatech(
+//       div({
+//         firstNumber: firstNumberInput,
+//         secondNumber: secondNumberInput,
+//       })
+//     );
+//   }
+
+//   return (
+//     <div className=" pt-54 w-1/2 mx-auto">
+//       <label className="block" htmlFor="First Number">
+//         First Number
+//       </label>
+//       <input
+//         type="text"
+//         value={firstNumberInput}
+//         onChange={(e) => setFirstNumberInput(e.target.value)}
+//         id="First Number"
+//         className="block border-[1px] border-black"
+//       />
+//       <label className="block" htmlFor="Second Number">
+//         Second Number
+//       </label>
+//       <input
+//         type="text"
+//         value={secondNumberInput}
+//         onChange={(e) => setSecondNumberInput(e.target.value)}
+//         id="Second Number"
+//         className="block border-[1px] border-black"
+//       />
+
+//       <button
+//         className=" cursor-pointer bg-teal-500 p-2 mt-2 block"
+//         onClick={handleSum}
+//       >
+//         Add
+//       </button>
+//       <button
+//         className=" cursor-pointer bg-teal-500 p-2 mt-2 block"
+//         onClick={handleSub}
+//       >
+//         Sub
+//       </button>
+//       <button
+//         className=" cursor-pointer bg-teal-500 p-2 mt-2 block"
+//         onClick={handleMlti}
+//       >
+//         Mlti
+//       </button>
+//       <button
+//         className=" cursor-pointer bg-teal-500 p-2 mt-2 block"
+//         onClick={handleDiv}
+//       >
+//         Div
+//       </button>
+//       <h1>{resultState}</h1>
+//     </div>
+//   );
+// }
+import React from 'react'
 
 export default function About() {
-  const [firstNumberInput, setFirstNumberInput] = useState(null);
-  const [secondNumberInput, setSecondNumberInput] = useState(null);
-  const [result, setResult] = useState(null);
-  const resultState = useSelector((state) => {
-    console.log("Hello", state);
-    return state.counter.result;
-  });
-  const dispatech = useDispatch();
-
-  function handleSum() {
-    // const result = Number(firstNumberInput) + Number(secondNumberInput);
-    // setResult(result);
-    dispatech(
-      add({
-        firstNumber: firstNumberInput,
-        secondNumber: secondNumberInput,
-      })
-    );
-  }
-  function handleSub() {
-    dispatech(
-      sub({
-        firstNumber: firstNumberInput,
-        secondNumber: secondNumberInput,
-      })
-    );
-  }
-  function handleMlti() {
-    dispatech(
-      mult({
-        firstNumber: firstNumberInput,
-        secondNumber: secondNumberInput,
-      })
-    );
-  }
-  function handleDiv() {
-    dispatech(
-      div({
-        firstNumber: firstNumberInput,
-        secondNumber: secondNumberInput,
-      })
-    );
-  }
-
   return (
-    <div className=" pt-54 w-1/2 mx-auto">
-      <label className="block" htmlFor="First Number">
-        First Number
-      </label>
-      <input
-        type="text"
-        value={firstNumberInput}
-        onChange={(e) => setFirstNumberInput(e.target.value)}
-        id="First Number"
-        className="block border-[1px] border-black"
-      />
-      <label className="block" htmlFor="Second Number">
-        Second Number
-      </label>
-      <input
-        type="text"
-        value={secondNumberInput}
-        onChange={(e) => setSecondNumberInput(e.target.value)}
-        id="Second Number"
-        className="block border-[1px] border-black"
-      />
-
-      <button
-        className=" cursor-pointer bg-teal-500 p-2 mt-2 block"
-        onClick={handleSum}
-      >
-        Add
-      </button>
-      <button
-        className=" cursor-pointer bg-teal-500 p-2 mt-2 block"
-        onClick={handleSub}
-      >
-        Sub
-      </button>
-      <button
-        className=" cursor-pointer bg-teal-500 p-2 mt-2 block"
-        onClick={handleMlti}
-      >
-        Mlti
-      </button>
-      <button
-        className=" cursor-pointer bg-teal-500 p-2 mt-2 block"
-        onClick={handleDiv}
-      >
-        Div
-      </button>
-      <h1>{resultState}</h1>
-    </div>
-  );
+    <div>About</div>
+  )
 }
